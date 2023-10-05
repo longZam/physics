053c09ffa093019839f5fcf0dca9756caecf1e17
longZam <longZam305@gmail.com>
2023-10-06 05:53:48 +0900

    refactor: improved AABB computation functions
    - AABB intersect method has been extracted from the ComputePenetration function
    - Added a constructor to the AABB struct for initializing member variables.

--- src/Physics/AABB.cs
+++ src/Physics/AABB.cs
@@ -12,6 +12,26 @@ public struct AABB
     public Vector2 Extends => max - min;
 
 
+    public AABB(Vector2 min, Vector2 max)
+    {
+        this.min = min;
+        this.max = max;
+    }
+
+    /// <summary>
+    /// 두 AABB의 겹침 여부 계산
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static bool Intersect(AABB a, AABB b)
+    {
+        if (a.max.x <= b.min.x || a.min.x >= b.max.x) return false;
+        if (a.max.y <= b.min.y || a.min.y >= b.max.y) return false;
+
+        return true;
+    }
+
     /// <summary>
     /// b에 대한 a의 침투 벡터를 계산
     /// </summary>
@@ -25,9 +45,9 @@ public struct AABB
         direction = Vector2.zero;
         distance = Fix64.Zero;
 
-        // 겹침 여부 판정
-        if (a.max.x <= b.min.x || a.min.x >= b.max.x) return false;
-        if (a.max.y <= b.min.y || a.min.y >= b.max.y) return false;
+        // 겹치지 않으면 false
+        if (!Intersect(a, b))
+            return false;
 
         // x, y축에 각각 투영한 길이 재기
         Fix64 overlapX = FixedMath.NET.Math.Max(a.max.x, b.max.x) - FixedMath.NET.Math.Min(a.min.x, b.min.x);
@@ -35,9 +55,9 @@ public struct AABB
         // 교집합 구하기
         overlapX = a.Extends.x + b.Extends.x - overlapX;
         overlapY = a.Extends.y + b.Extends.y - overlapY;
-        // 방향성 
-        overlapX *= a.Center.x < b.Center.x ? -Fix64.One : Fix64.One;
-        overlapY *= a.Center.y < b.Center.y ? -Fix64.One : Fix64.One;
+        // 방향성
+        overlapX *= (Fix64)Fix64.Sign(a.Center.x - b.Center.x);
+        overlapY *= (Fix64)Fix64.Sign(a.Center.y - b.Center.y);
 
         // out 인자 채우기
         Vector2 penetration;
