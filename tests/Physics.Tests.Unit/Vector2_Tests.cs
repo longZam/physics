39588e9dc07632f3cf1c4055a94e713286e6c98a
longZam <longZam305@gmail.com>
2023-10-08 12:40:25 +0900

    test: Vector2_Tests.IntersectSuccess 테스트에 정/역방향 정상 작동 검증 추가

--- tests/Physics.Tests.Unit/Vector2_Tests.cs
+++ tests/Physics.Tests.Unit/Vector2_Tests.cs
@@ -6,7 +6,7 @@ namespace Physics.Tests.Unit;
 public class Vector2_Tests
 {
     [Fact]
-    public void Intersect()
+    public void IntersectSuccess()
     {
         Vector2 a0 = new Vector2(0, 0);
         Vector2 a1 = new Vector2(1, 1);
@@ -14,5 +14,8 @@ public class Vector2_Tests
         Vector2 b1 = new Vector2(1, 0);
 
         Assert.True(Vector2.Intersect(a0, a1, b0, b1));
+        Assert.True(Vector2.Intersect(a1, a0, b0, b1));
+        Assert.True(Vector2.Intersect(a0, a1, b1, b0));
+        Assert.True(Vector2.Intersect(a1, a0, b1, b0));
     }
 }
